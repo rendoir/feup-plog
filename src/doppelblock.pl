3ea52d3cc161f36bbdacdf4b57410f11260f9471
rendoir <danfil.marques@gmail.com>
2017-12-21 16:44:58 +0000

    Added elapsed time

--- src/doppelblock.pl
+++ src/doppelblock.pl
@@ -2,8 +2,9 @@
 :- include('board.pl').
 :- include('display.pl').
 
-%doppelblock([4,8,4,5,6,5],[9,7,2,10,3,1]).
+%doppelblock(6,[4,8,4,5,6,5],[9,7,2,10,3,1],Board).
 doppelblock(Size, TopSums, LeftSums, Board) :- !,
+  statistics(runtime, [T0|_]),
   checkArguments(Size, TopSums, LeftSums),
   initBoard(Board, Size),
   transpose(Board, TransposeBoard),
@@ -11,11 +12,15 @@ doppelblock(Size, TopSums, LeftSums, Board) :- !,
   applySumsConstrains(Board, LeftSums),
   applySumsConstrains(TransposeBoard, TopSums),
   label(Board),
+  statistics(runtime, [T1|_]),
+  ElapsedTime is T1 - T0,
+  format('Solution found in: ~3d seconds', [ElapsedTime]), nl,
   drawBoard(Board, TopSums, LeftSums, Size).
 doppelblock(_, _, _, _) :-
   printUsage, !.
 
 doppelblock(Size, TopSums, LeftSums, Board, LabelingOptions) :- !,
+  statistics(runtime, [T0|_]),
   checkArguments(Size, TopSums, LeftSums),
   initBoard(Board, Size),
   transpose(Board, TransposeBoard),
@@ -23,18 +28,12 @@ doppelblock(Size, TopSums, LeftSums, Board, LabelingOptions) :- !,
   applySumsConstrains(Board, LeftSums),
   applySumsConstrains(TransposeBoard, TopSums),
   label(Board, LabelingOptions),
+  statistics(runtime, [T1|_]),
+  ElapsedTime is T1 - T0,
+  format('Solution found in: ~3d seconds', [ElapsedTime]), nl,
   drawBoard(Board, TopSums, LeftSums, Size).
 doppelblock(_, _, _, _, _) :-
   printUsage, !.
-
-test(List, Size, Sum) :-
-  length(List, Size),
-  Domain is Size - 2,
-  domain(List, 0, Domain),
-  count(0, List, #=, 2),
-  setDistinctValues(List, Domain, 1),
-  sumConstraint(List, Sum),
-  labeling([], List).
 
 printAllSolutions(OutputFile, Time, R, [Size, TopSums, LeftSums, Board]) :-
   open(OutputFile, write, X),
