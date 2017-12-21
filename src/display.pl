a0efea287bd17f48a3f8429bdd4417b941ff05fa
rendoir <danfil.marques@gmail.com>
2017-12-21 18:42:26 +0000

    Added documentation

--- src/display.pl
+++ src/display.pl
@@ -1,27 +1,40 @@
-/**
+/*
   display.pl
 
-  This file is responsible for the Console Display of the board.
-**/
+  This file is responsible for the console interface of the program.
+*/
 
 
-drawBoard(Board, TopSums, LeftSums, Size) :-
+/*
+  drawBoard(+Board, +TopSums, +RightSums, +Size)
+    @brief Prints the board to the screen
+
+	@param Board - The matrix (inner board)
+	@param TopSums - The sums on top of the board
+	@param RightSums - The sums on the right of the board
+	@param Size - The size of the matrix
+*/
+drawBoard(Board, TopSums, RightSums, Size) :-
   write('   '), drawTopSums(TopSums), nl,
   write('   '), drawHeader(Size), nl,
-  drawBoardRest(Board, LeftSums, Size), !.
-
-
-drawBoardRest([], _, _).
-drawBoardRest([Row | Rest], [Sum | OtherSums], Size) :-
+  drawBoard(Board, RightSums, Size), !.
+drawBoard([], _, _).
+drawBoard([Row | Rest], [Sum | OtherSums], Size) :-
   drawUpSeparatorLine(Size),
   write('  |'),
   drawRow(Row), 
   write(' '),
   write(Sum), nl,
   drawDownSeparatorLine(Size),
-  drawBoardRest(Rest, OtherSums, Size).
+  drawBoard(Rest, OtherSums, Size).
 
 
+/*
+  drawRow(+Row)
+    @brief Prints a row of the board
+
+	@param Row - A row of the board
+*/
 drawRow([]).
 drawRow([Cell | Remainder]) :-
   write('  '),
@@ -30,6 +43,12 @@ drawRow([Cell | Remainder]) :-
   drawRow(Remainder).
 
 
+/*
+  drawUpSeparatorLine(+Size)
+    @brief Prints the upper separator between rows
+
+	@param Size - The size of the matrix
+*/
 drawUpSeparatorLine(Size) :-
   write('  |'),
   drawUpSeparatorLine(Size, Size), nl.
@@ -40,6 +59,12 @@ drawUpSeparatorLine(Size, Counter) :-
   drawUpSeparatorLine(Size, NextCounter).
 
 
+/*
+  drawDownSeparatorLine(+Size)
+    @brief Prints the bottom separator between rows
+
+	@param Size - The size of the matrix
+*/
 drawDownSeparatorLine(Size) :-
   write('  |'),
   drawDownSeparatorLine(Size, Size), nl.
@@ -50,6 +75,12 @@ drawDownSeparatorLine(Size, Counter) :-
   drawDownSeparatorLine(Size, NextCounter).
 
 
+/*
+  drawHeader(+Size)
+    @brief Prints the separator between the TopSums and the matrix
+
+	@param Size - The size of the matrix
+*/
 drawHeader(Size) :-
   drawHeader(Size, Size).
 drawHeader(_, 0).
@@ -59,26 +90,37 @@ drawHeader(Size, Counter) :-
   drawHeader(Size, NextCounter).
 
 
+/*
+  drawTopSums(+TopSums)
+    @brief Prints the top sums
+
+	@param TopSums - The sums on top of the board
+*/
 drawTopSums([]).
 drawTopSums([Sum | Rest]) :-
   write('  '), write(Sum), write('   '),
   drawTopSums(Rest).
 
 
-/**
-  drawCell/2: Draws a board cell
-    The black cells have the values '0'.
-**/
+/*
+  drawCell(+Cell)
+    @brief Prints a cell. If the cell is zero, it is considered a black cell.
+
+	@param Cell - A matrix element
+*/
 drawCell(0) :-
   write('X').
 drawCell(Cell) :-
   write(Cell).
 
-
+/*
+  printUsage
+    @brief Prints the usage in case the arguments are wrong.
+*/
 printUsage :-
-  write('Usage: doppelblock(?Size, ?TopSums, ?LeftSums, ?Board, (Optinal) +LabelingOptions).'), nl,
+  write('Usage: doppelblock(?Size, ?TopSums, ?RightSums, ?Board, [+LabelingOptions]).'), nl,
   write('  Size            - Integer      - The Size of the Board'), nl,
   write('  TopSums         - Integer List - The sums on top of the board'), nl,
-  write('  LeftSums        - Integer List - The sums on the left of the board'), nl,
+  write('  RightSums       - Integer List - The sums on the left of the board'), nl,
   write('  Board           - Integer List - The Board correspondent to the sums'), nl,
-  write('  LabelingOptions - Integer List - Optins to labeling the variables'), nl.
+  write('  LabelingOptions - Atom List    - Options to labeling the variables'), nl.
--- src/doppelblock.pl
+++ src/doppelblock.pl
@@ -1,48 +1,78 @@
+/*
+  doppelblock.pl
+
+  This file holds the predicates that are entry points to the solver.
+*/
+
 :- use_module(library(timeout)).
 :- include('board.pl').
 :- include('display.pl').
 
-%doppelblock(6,[4,8,4,5,6,5],[9,7,2,10,3,1],Board).
-doppelblock(Size, TopSums, LeftSums, Board) :- !,
-  statistics(runtime, [T0|_]),
-  checkArguments(Size, TopSums, LeftSums),
-  initBoard(Board, Size),
-  transpose(Board, TransposeBoard),
-  initBoard(TransposeBoard, Size),
-  applySumsConstrains(Board, LeftSums),
-  applySumsConstrains(TransposeBoard, TopSums),
-  label(Board),
-  statistics(runtime, [T1|_]),
-  ElapsedTime is T1 - T0,
-  format('Solution found in: ~3d seconds', [ElapsedTime]), nl,
-  drawBoard(Board, TopSums, LeftSums, Size).
+
+/*
+  doppelblock(?Size, ?TopSums, ?RightSums, ?Board)
+    @brief Solves an instance of the puzzle
+
+    @param Size - The size of the matrix
+	@param TopSums - The sums on top of the board
+	@param RightSums - The sums on the right of the board
+	@param Board - The matrix (inner board)
+  
+    Example: doppelblock(6, [4,8,4,5,6,5], [9,7,2,10,3,1], Board).
+*/
+doppelblock(Size, TopSums, RightSums, Board) :- !,
+  doppelblock(Size, TopSums, RightSums, Board, []).
 doppelblock(_, _, _, _) :-
   printUsage, !.
 
-doppelblock(Size, TopSums, LeftSums, Board, LabelingOptions) :- !,
+
+/*
+  doppelblock(?Size, ?TopSums, ?RightSums, ?Board, +LabelingOptions)
+    @brief Solves an instance of the puzzle
+
+    @param Size - The size of the matrix
+	@param TopSums - The sums on top of the board
+	@param RightSums - The sums on the right of the board
+	@param Board - The matrix (inner board)
+	@param LabelingOptions - Option list for the labeling/2 predicate
+  
+    Example: doppelblock(6, [4,8,4,5,6,5], [9,7,2,10,3,1], Board, [ffc]).
+*/
+doppelblock(Size, TopSums, RightSums, Board, LabelingOptions) :- !,
   statistics(runtime, [T0|_]),
-  checkArguments(Size, TopSums, LeftSums),
+  checkArguments(Size, TopSums, RightSums),
   initBoard(Board, Size),
   transpose(Board, TransposeBoard),
   initBoard(TransposeBoard, Size),
-  applySumsConstrains(Board, LeftSums),
+  applySumsConstrains(Board, RightSums),
   applySumsConstrains(TransposeBoard, TopSums),
   label(Board, LabelingOptions),
   statistics(runtime, [T1|_]),
   ElapsedTime is T1 - T0,
   format('Solution found in: ~3d seconds', [ElapsedTime]), nl,
-  drawBoard(Board, TopSums, LeftSums, Size).
+  drawBoard(Board, TopSums, RightSums, Size).
 doppelblock(_, _, _, _, _) :-
   printUsage, !.
 
-printAllSolutions(OutputFile, Time, R, [Size, TopSums, LeftSums, Board]) :-
+
+/*
+  printAllSolutions(+OutputFile, +Time, -Result, ?Game)
+    @brief Writes all the solutions generated during a timeout
+
+    @param OutputFile - The file to write the results to
+	@param Time - Timeout in milliseconds (the predicates end once it is reached)
+	@param Result - The timout result
+	@param Game - A list representing an instance of the game
+  
+    Example: printAllSolutions('solutions.txt', 5000, Result, [6, _, _, _]).
+*/
+printAllSolutions(OutputFile, Time, Result, [Size, TopSums, RightSums, Board]) :-
   open(OutputFile, write, X),
   current_output(CO),
   set_output(X),
-  printAllSolutions(Time, R, [Size, TopSums, LeftSums, Board]),
+  printAllSolutions(Time, Result, [Size, TopSums, RightSums, Board]),
   close(X),
   set_output(CO).
-
-printAllSolutions(Time, R, [Size, TopSums, LeftSums, Board]) :-
-  time_out((doppelblock(Size, TopSums, LeftSums, Board), fail), Time, R).
+printAllSolutions(Time, Result, [Size, TopSums, RightSums, Board]) :-
+  time_out((doppelblock(Size, TopSums, RightSums, Board), fail), Time, Result).
 printAllSolutions(_, _, _).
